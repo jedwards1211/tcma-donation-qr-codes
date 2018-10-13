73d2dfedd7a247c78b9c3ab9c7a12a94df361d60
Andy Edwards <jedwards@fastmail.com>
2018-10-12 23:48:26 -0500

    Updates

---
+++ precache-manifest.8c8966eb139ea780daa34ed14b06e552.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "126be299ffebb2c7988d0464d7fb6ea5",
+    "url": "/tcma-donation-qr-codes/static/media/logo.126be299.png"
+  },
+  {
+    "revision": "4fb8a3af765426562cbe",
+    "url": "/tcma-donation-qr-codes/static/js/runtime~main.4fb8a3af.js"
+  },
+  {
+    "revision": "e910f93f2d5d3381f302",
+    "url": "/tcma-donation-qr-codes/static/js/main.e910f93f.chunk.js"
+  },
+  {
+    "revision": "5225b1a7fd63fb405178",
+    "url": "/tcma-donation-qr-codes/static/js/1.5225b1a7.chunk.js"
+  },
+  {
+    "revision": "e910f93f2d5d3381f302",
+    "url": "/tcma-donation-qr-codes/static/css/main.e55fd7ac.chunk.css"
+  },
+  {
+    "revision": "ed387436cf8e294be35a58d8feb27350",
+    "url": "/tcma-donation-qr-codes/index.html"
+  }
+];
