956f2c48009fd455398e0be710bdbf7e61382d41
Andy Edwards <jedwards@fastmail.com>
2018-10-12 23:42:33 -0500

    deploy: add docs folder

---
+++ docs/precache-manifest.cf723befbd68e7f9e3258a96ad1ce84d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "126be299ffebb2c7988d0464d7fb6ea5",
+    "url": "/static/media/logo.126be299.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3ac0a0550a2b46971d5b",
+    "url": "/static/js/main.3ac0a055.chunk.js"
+  },
+  {
+    "revision": "61fe0a05dd70bef278e1",
+    "url": "/static/js/1.61fe0a05.chunk.js"
+  },
+  {
+    "revision": "3ac0a0550a2b46971d5b",
+    "url": "/static/css/main.e55fd7ac.chunk.css"
+  },
+  {
+    "revision": "33b3961c33537eed45e0a183a9b8b5d9",
+    "url": "/index.html"
+  }
+];
